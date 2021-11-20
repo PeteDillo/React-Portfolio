f5676307fe9f5b0c3526e79d24eff7b4db2c96fd
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-11-19 23:22:09 -0600

    updated the fonts + created a hook to update the navbar + added styling and icons to the topbar.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,14 @@ import Works from './Works/Works'
 import Testimonials from './Testimonials/Testimonials'
 import Contact from './Contact/Contact'
 import './app.scss'
+import React, { useState } from 'react';
+
 
 function App(){
+    const [menuOpen,setMenuOpen] = useState(false)
     return (
         <div className='app'>
-            <Topbar/>
+            <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
             <div className="sections">
                 <Intro/>
                 <Portfolio/>
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,28 +1,31 @@
-import './topbar.scss'
-import {Person, Mail} from "@material-ui/icons"
-export default function topbar() {
-    return (
-        <div className="topbar">
-            <div className='wrapper'>
-                <div className="left">
-                    <a href="#intro"className='logo'>
-                        pedro.
-                    </a>
-                    <div className="itemContainer">
-                    <Person className="icon"/>
-                    <span>212-316-5584</span>
-                    </div>
-                    <div className="itemContainer">
-                    <Mail className="icon" />
-                    <span>
-                        pete@mail.net
-                    </span>
-                </div>
-                </div>
-                <div className="right">
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
-                </div>
-            </div>
+export default function Topbar({menuOpen, setMenuOpen}) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+          pedro.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>212-316-5584</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>pete@mail.net</span>
+          </div>
         </div>
-    )
+        <div className="right">
+          <div className="dashes" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
