46e685f5037aeb33674f8c9b6613db2ac8b2c982
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-01-20 14:16:01 -0600

    added whenever to click on a menu item the menu closes fixed a typo in the testimonials component started added picture of myself and styling

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,6 +3,12 @@ import './intro.scss'
 export default function intro() {
     return (
         <div className='intro' id='intro'>
+            <div className="left">
+                <div className="imgContainter">
+                    <img src="assets/pedro.png" alt="A Picture of Pedro standing with his arms crossed wearing a white hoodie"></img>
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,8 @@
-import './testimonials.scss'
+import "./testimonials.scss";
 
 export default function Testimonials() {
-    return (
-        <div className='testimonials' id='testimonials'>
-            
-        </div>
-    )
+  return (
+    <div className="testimonials" id="testimonials">
+    </div>
+  );
 }
